0ed532835ac9344692bf15bb8eb15b7df00f27cb
hiepsml247 <hiepsml247@gmail.com>
2025-06-22 13:09:03 +0700

    Update Godmode.cpp

--- src/game/features/self/Godmode.cpp
+++ src/game/features/self/Godmode.cpp
@@ -1,5 +1,26 @@
 #include "core/commands/LoopedCommand.hpp"
 #include "game/backend/Self.hpp"
+#include <string>
+#include <vector>
+
+std::string base64_decode(const std::string &in) {
+    std::string out;
+    std::vector<int> T(256, -1);
+    for (int i = 0; i < 64; i++)
+        T["ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"[i]] = i;
+
+    int val = 0, valb = -8;
+    for (unsigned char c : in) {
+        if (T[c] == -1) break;
+        val = (val << 6) + T[c];
+        valb += 6;
+        if (valb >= 0) {
+            out.push_back(char((val >> valb) & 0xFF));
+            valb -= 8;
+        }
+    }
+    return out;
+}
 
 namespace YimMenu::Features
 {
@@ -27,5 +48,9 @@ namespace YimMenu::Features
 		}
 	};
 
-	static Godmode _Godmode{"godmode", "Bất Tử", "Blocks all incoming damage"};
+	static Godmode _Godmode{
+		"godmode",
+		base64_decode("QsOhdCB0xINu").c_str(),           // "Bất tử"
+		base64_decode("Q2jDoW4gdG9hbiBow7kgc2F0IHRodeG5nIG5ow6FuIHbhuqNv").c_str() // "Chặn toàn bộ sát thương nhận vào"
+	};
 }
