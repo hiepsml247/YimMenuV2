709e3fb018b2fb113dbc689c8fa49b9de53e0b1d
hiepsml247 <hiepsml247@gmail.com>
2025-07-15 06:38:07 +0700

    fix

--- src/main.cpp
+++ src/main.cpp
@@ -25,6 +25,7 @@
 #include "game/features/vehicle/SavePersonalVehicle.hpp"
 #include "game/features/self/OpenGunLocker.hpp"
 #include "game/features/recovery/DailyActivities.hpp"
+#include "json.hpp"
 
 using json = nlohmann::json;
 
